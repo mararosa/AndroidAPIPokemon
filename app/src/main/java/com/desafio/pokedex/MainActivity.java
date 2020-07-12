9e38ae3f5a9fb007f64a0779d7d69bdd658272b5
Edimara Roberta Rosa <edimara.rosa@hotmail.com>
2020-07-12 14:12:38 -0300

    add retrofit build and method getdata

--- app/src/main/java/com/desafio/pokedex/MainActivity.java
+++ app/src/main/java/com/desafio/pokedex/MainActivity.java
@@ -9,10 +9,22 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Retrofit retrofit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl("https://pokeapi.co/api/v2/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        getData();
+    }
+
+    private void getData() {
+
     }
 }
