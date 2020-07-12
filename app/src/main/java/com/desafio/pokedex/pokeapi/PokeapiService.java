f5cdc8c5ad75f58c2620a28a781bfd46cc5e890a
Edimara Roberta Rosa <edimara.rosa@hotmail.com>
2020-07-12 14:27:25 -0300

    add action to the interface

--- app/src/main/java/com/desafio/pokedex/pokeapi/PokeapiService.java
+++ app/src/main/java/com/desafio/pokedex/pokeapi/PokeapiService.java
@@ -3,8 +3,10 @@ package com.desafio.pokedex.pokeapi;
 import com.desafio.pokedex.models.PokemonResponse;
 
 import retrofit2.Call;
+import retrofit2.http.GET;
 
 public interface PokeapiService {
 
+    @GET("pokemon")
     Call<PokemonResponse> getListPokemon();
 }
