015e34dbfd23149ecf71114ba7360685cffc2960
Edimara Roberta Rosa <edimara.rosa@hotmail.com>
2020-07-12 14:10:51 -0300

    add retrofit and gson dependencies and internet permission

---
+++ app/src/main/java/com/desafio/pokedex/MainActivity.java
@@ -0,0 +1,18 @@
+package com.desafio.pokedex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
