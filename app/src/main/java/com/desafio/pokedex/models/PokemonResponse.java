f797f7e39628532a8f2addcec187e3d5345afa5e
Edimara Roberta Rosa <edimara.rosa@hotmail.com>
2020-07-12 14:26:10 -0300

    add getter and setter

--- app/src/main/java/com/desafio/pokedex/models/PokemonResponse.java
+++ app/src/main/java/com/desafio/pokedex/models/PokemonResponse.java
@@ -5,4 +5,12 @@ import java.util.ArrayList;
 public class PokemonResponse {
 
     private ArrayList<Pokemon> results;
+
+    public ArrayList<Pokemon> getResults() {
+        return results;
+    }
+
+    public void setResults(ArrayList<Pokemon> results) {
+        this.results = results;
+    }
 }
